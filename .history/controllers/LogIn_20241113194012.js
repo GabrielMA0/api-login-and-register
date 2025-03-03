a055dae1192d5f3d171eed8fcbc6ba834c1ad2c5
Gabriel Moreira <moreira.araujo99@gmail.com>
2025-03-03 15:37:39 -0300

    fix: substituido bycrypt pelo do js puro

--- .history/controllers/LogIn_20241113194012.js
+++ .history/controllers/LogIn_20241113194012.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 import { z } from 'zod';
 
 const LogIn = async (req) => {
--- .history/controllers/LogIn_20250303153134.js
+++ .history/controllers/LogIn_20250303153134.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { z } = require('zod');
 
 const LogIn = async (req) => {
